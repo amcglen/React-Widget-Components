b4daa8a29e3e092ac36c993b4eabdcd55e8952be
Andrew McGlen <andy_mac82@hotmail.co.uk>
2021-08-20 05:41:49 +0100

    Added Dropdown component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Accordion from './Components/Accordion';
 import Search from './Components/Search';
+import Dropdown from './Components/Dropdown';
 
   const items =[
     {title: "What is React?", content: "React is a front-end JS framework."},
@@ -8,11 +9,26 @@ import Search from './Components/Search';
     {title: "How do you use React?", content: "You use React by creating components."}
   ];
 
+  const options= [
+    {label:"The Colour Blue", value:"blue"},
+    {label:"The Colour Green", value:"green"},
+    {label:"The Colour Red", value:"red"},
+  ];
+
 const App = ()=>{
+
+const [selected, setSelected] = useState(options[0]);
+
+
   return (
     <div className="ui container">
       {/* <Accordion items={items} /> */}
-      <Search />
+      {/* <Search /> */}
+      <Dropdown 
+        options={options} 
+        selected={selected} 
+        onSelectedChange={setSelected} 
+      />
     </div>
     
   );
---
+++ src/Components/Dropdown.js
@@ -0,0 +1,69 @@
+import React, {useEffect, useState, useRef} from 'react';
+
+const Dropdown = ({options, selected, onSelectedChange})=>{
+  const [open, setOpen] = useState(false);
+
+  const ref = useRef();
+
+  //needs to run onl
+  useEffect(()=>{
+
+    const onBodyClick = (event) => {
+      if(ref.current.contains(event.target)){
+        return;
+      }
+      setOpen(false);
+    };
+
+    document.body.addEventListener('click', onBodyClick, {capture: true});
+      
+
+    //clean up functions
+    return () => {  
+      document.body.removeEventListener('click', onBodyClick, {capture: true});
+    };
+  }, []);
+
+
+  //display the memu options from the options lists
+  const renderOptions = options.map((option)=>{
+    //remove the currently selected option for the dropdown list
+    if(option.value === selected.value){
+      return null;
+    }
+
+    //
+    return(
+    <div 
+      key={option.value} 
+      className="item"
+      onClick={()=>onSelectedChange(option)}
+    >
+      {option.label}
+    </div>
+    );
+  });
+
+  console.log(ref.current);
+
+  return(
+    <div ref={ref} className="ui form">
+      <div className="field">
+        <label className="label">Select a Colour</label>
+        <div 
+          onClick={()=> setOpen(!open)} 
+          className={`ui selection dropdown ${open ? 'visible active' : ''}`}
+        >
+          <i className="dropdown icon"></i>
+          <div className="text">{selected.label}</div>
+          <div className={`menu ${open ? 'visible transition' : ''}`}>
+            {renderOptions}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+export default Dropdown;
